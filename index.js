5ce6df9f7d0ce21cacd518d8e98dbb2598357bd3
TheRealEnvist <137974409+TheRealEnvist@users.noreply.github.com>
2025-02-03 21:03:28 -0500

    fixed

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ async function onLoad(){
             serverClone.hidden = false
             serverClone.querySelector('#serverID').textContent = data["id"];
             document.getElementById("severDisplayList").appendChild(serverClone)
-            loadPlayers(serverClone, params.get("gameID"),data["id"]);
+            loadPlayers(serverClone, gameID,data["id"]);
         });
     }else{
         document.getElementById("serverError").hidden = false;
